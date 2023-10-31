424790fe9018894ddf524a36e390024e1c29f671
saarizaws <saad8840@gmail.com>
2023-10-31 12:08:33 +0500

    first commit

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -37,8 +37,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptatum.
+            Quality above the industry standards!
           </p>
           <div>
             <Link
